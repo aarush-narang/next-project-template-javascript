a658d61d58739998d93446e0066106e20122c6a2
aarush-narang <aarushnarang@gmail.com>
2022-10-20 19:39:31 -0700

    add swr

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script src="https://www.desmos.com/api/v1.7/calculator.js?apiKey=dcb31709b452b1cf9dc26972add0fda6"></script>
       </Head>
       <body>
         <Main />
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
 export default function Home() {
-    return <div className="text-orange-500">asd</div>;
+    return <div>Hello World</div>;
 }
